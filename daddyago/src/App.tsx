733117b5b7de1e051e294775f8acca6f19551dd4
Thomas Voss <mail@thomasvoss.com>
2025-11-21 14:17:36 +0100

    Add a semicircle chart

--- daddyago/src/App.tsx
+++ daddyago/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import HelloWorld from './HelloWorld';
+import SemiCircleChart from './SemiCircleChart';
 
 const App: React.FC = () => {
 	return (
 		<div>
 			<h1>Daddyago’s Migrate Predictor</h1>
 			<HelloWorld />
+			<SemiCircleChart percentage={0.75} />
 		</div>
 	);
 };
---
+++ daddyago/src/SemiCircleChart.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './SemiCircleChart.css';
+
+type ChartProps = {
+	percentage: number;
+};
+
+const SemiCircleChart: React.FC<ChartProps> = ({ percentage }) => {
+	const angle = percentage * 180;
+	const style = {'--angle': angle + 'deg'} as React.CSSProperties;
+
+	return (
+		<div className="sc-gauge">
+			<div className="sc-background">
+				<div className="sc-percentage" style={style}></div>
+				<div className="sc-mask"></div>
+				<span className="sc-value">{ percentage * 100 }%</span>
+			</div>
+		</div>
+	);
+};
+
+export default SemiCircleChart;
