ef90968c26f71d50d018b9be3f1e252abaa46dfa
Abhinav Mudugu <am225mq@student.lnu.se>
2025-11-21 15:43:23 +0100

    finished csv connector

--- backend/modelConnector.py
+++ backend/modelConnector.py
@@ -3,11 +3,14 @@ import pandas as pd
 
 class RegressorModelConnector():
     model: CatBoostRegressor
-    def get_prediction(self, X_val_json):
-        X_val = pd.read_json(X_val_json)
+    def get_prediction(self, health_val_json, person_val_json):
+        h_val = pd.read_json(health_val_json)
+        p_val = pd.read_json(person_val_json)
+
+        X_val = pd.merge(h_val, p_val, how="inner")
         self.load_model("migrain_probability.cbm")
         Y_val = self.model.predict(X_val)
-        probaility = Y_val[0] * 100
+        probaility = round(Y_val[0] * 100)
 
         return probaility
 
@@ -19,8 +22,11 @@ class RegressorModelConnector():
 
 class ClassifierModelConnector():
     model: CatBoostClassifier
-    def get_prediction(self, X_val_json):
-        X_val = pd.read_json(X_val_json)
+    def get_prediction(self, health_val_json, person_val_json):
+        h_val = pd.read_json(health_val_json)
+        p_val = pd.read_json(person_val_json)
+        X_val = pd.merge(h_val, p_val, how="inner")
+        
         self.load_model("migrain_type.cbm")
         Y_val = self.model.predict(X_val)
         return Y_val[0]
