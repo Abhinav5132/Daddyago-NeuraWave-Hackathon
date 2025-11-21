76e619f0a8cf2e7ac80d0e5a519c70f5346a09fe
Thomas Voss <mail@thomasvoss.com>
2025-11-21 13:49:58 +0100

    Create a basic frontend

---
+++ daddyago/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import HelloWorld from './HelloWorld';
+
+const App: React.FC = () => {
+	return (
+		<div>
+			<h1>Daddyago</h1>
+			<HelloWorld />
+		</div>
+	);
+};
+
+export default App;
